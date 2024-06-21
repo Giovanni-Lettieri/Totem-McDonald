25e134ee9fbcada24ac1ace6283e4ac6855a02e1
Giovanni Lettieri <Giovanni.Lettieri05@gmail.com>
2024-06-21 17:00:31 +0200

    feat: animazione loco e back + inizio animazione category

--- my-app/src/left-col/left-col.component.ts
+++ my-app/src/left-col/left-col.component.ts
@@ -4,15 +4,42 @@ import { ProdottiComponent } from "../prodotti/prodotti.component";
 import { ChangeLanguagesService } from '../Service/change-languages.service';
 import { Subscription } from 'rxjs';
 import { LightDarkServiceService } from '../Service/light-dark-service.service';
-
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
     selector: 'left-col',
     standalone: true,
     templateUrl: './left-col.component.html',
     styleUrl: './left-col.component.css',
-    imports: [CategoryComponent, ProdottiComponent]
+    imports: [CategoryComponent, ProdottiComponent , CommonModule],
+    animations: [
+        trigger('animazioneLogo', [
+          
+          state('start', style({ transform: 'translatey(0%)'  })),
+          
+          state('end', style({ transform: 'translatey(-400%)' })),
+          
+          transition('start => end', [
+            animate('500ms ease-out', style({ transform: 'translatey(-400%)' }))
+          ]),
+          transition('end => start', [
+            animate('500ms ease-in', style({ transform: 'translatey(0%)' }))
+          ]),
+        ])
+      ]
 })
 export class LeftColComponent{
+
+    backVisible : boolean = false
+    animazioneLogo : boolean = true
+    nascondiTopCont() {
+        this.backVisible = true
+        this.animazioneLogo = false
+    }
+    back(){
+        this.backVisible = false
+        this.animazioneLogo = true    
+    }
     
     Ehi : String =  this.lingSer.getTesto().Ehi
     WU : String = this.lingSer.getTesto().WU
